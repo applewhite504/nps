978667baf90bcf3844f81dbbb7ee1709dc8f7093
Beth Applewhite <bethapplewhite.nola@gmail.com>
2020-03-04 14:15:21 -0600

    fixing and updating

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 'use strict';
-
-let nationalUrl = 'https://developer.nps.gov/api/v1/parks';
+let nationalUrl = 'https://developer.nps.gov/api/v1/campgrounds';
 let apiKey = 'rrspjQLxbINfED0AOxwHxMTwmbxi1IsWeqltUV1I';
-
 // This function formats the parameters for the api call
 function formatParams(params) {
     const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
         return queryItems.join('&');
 }
-
 // API Fetch for national parks
 function apiData(campgroundLocation){
     const paramsNational = {
         api_key: apiKey,
         stateCode: campgroundLocation,
+        maxResults: 3,
     }
-
     let queryString = formatParams(paramsNational);
     let newNationalUrl = nationalUrl + '?' + queryString;
-
     fetch(newNationalUrl)
     .then(response => response.json())
     .then(responseJson => displayApiData(responseJson))
@@ -27,34 +23,33 @@ function apiData(campgroundLocation){
         $('.displayError').html(`Sorry, but there were some issues behind the scenes. Please wait and try again: ${error.message}`)
     })
 }
-
+function getNumParks() {
+	return Number($('#numParks').val() || 0)
+}
 // Displays the data from the api call onto the html
 function displayApiData(responseJson){
  for(let i = 0; i < responseJson.data.length; i++){
+     console.log(responseJson.data[i]);
      $('.displayNational').append(`
-        <h2>${responseJson.data[i].fullName}</h2>
-        <h4>${responseJson.data[i].weatherInfo}</h4>
+        <h2>${responseJson.data[i].name}</h2>
+        <h4>${responseJson.data[i].weatheroverview}</h4>
         <p>${responseJson.data[i].description}</p>
-        <p>${responseJson.data[i].designation}</p>
-        <p>${responseJson.data[i].directionsInfo}</p>
-        <p><a href="${responseJson.data[i].directionsUrl}">${responseJson.data[i].directionsUrl}</a></p><br><br><hr>
+        <p>${responseJson.data[i].directionsoverview}</p>
+        <img src="${responseJson.data[i].images[0].url}" width=300>
+        <hr>
      `)
  }
 }
-
 // This function handles the form and button clicks in the html
 function formHandle (){
     $('#park-button').click(event => {
         event.preventDefault();
-        
+        //const numParks = getNumParks()
         let campgroundLocation = $('#submitNational').val();
         apiData(campgroundLocation);
-
         $('.displayNational').empty();
         $('.displayError').empty();
     });
 }
-
 // this calls the button click function
 $(formHandle);
-
